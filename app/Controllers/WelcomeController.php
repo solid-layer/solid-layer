8e9e351375ccd7476d9a6988f7cd3ecd99aa5f2d
Daison Carino <daison12006013@gmail.com>
2015-07-02 18:16:13 +0000

    changing the location of welcome page to main root

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -13,6 +13,6 @@ class WelcomeController extends Controller
 {
     public function showSignatureAction()
     {
-        return View::make('welcome.showSignature');
+        return View::make('welcome');
     }
 }
