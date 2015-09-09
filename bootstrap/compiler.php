e97f1e231b045a19411184933ee714e024f2e970
Daison Carino <daison12006013@gmail.com>
2015-09-09 14:38:37 +0000

    adding compiler config

---
+++ bootstrap/compiler.php
@@ -0,0 +1,21 @@
+<?php
+
+define('BASE_PATH', dirname(__DIR__));
+
+use ClassPreloader\ClassLoader;
+
+require_once BASE_PATH . '/vendor/classpreloader/classpreloader/src/ClassLoader.php';
+
+$config = ClassLoader::getIncludes(function (ClassLoader $loader) {
+    $loader->register();
+});
+
+$files = include BASE_PATH . '/config/compile.php';
+
+foreach ($files as $file) {
+    if ( file_exists($file) ) {
+        $config->addFile($file);
+    }
+}
+
+return $config;
