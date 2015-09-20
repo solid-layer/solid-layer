b9bed8d1c320ddaef3c5fc8c5e17ade2bfcc9c20
Daison Carino <daison12006013@gmail.com>
2015-09-20 03:07:18 +0000

    fixes on the render parameters, if empty the foreach loop should be ignored

--- bootstrap/src/Adapters/Blade/BladeAdapter.php
+++ bootstrap/src/Adapters/Blade/BladeAdapter.php
@@ -14,35 +14,38 @@ class BladeAdapter extends Engine
     public function __construct($view, $di)
     {
         parent::__construct($view, $di);
-    }
-
-    public function render($path, $params)
-    {
-        $blade_engine = new CompilerEngine(
+        $this->blade_engine = new CompilerEngine(
             new BladeCompiler( new Filesystem, config()->path->storage_views)
         );
-        $c = $blade_engine->getCompiler();
+    }
 
+    protected function compiler()
+    {
+        return $this->blade_engine->getCompiler();
+    }
 
+    public function render($path, $params = [])
+    {
         # - let's check if the blade file time is different
         # from the compiled file
 
-        if ($c->isExpired($path)) {
-            $c->compile($path);
+        if ($this->compiler()->isExpired($path)) {
+            $this->compiler()->compile($path);
         }
 
 
         # - now buffer the compiled template to get the php variables
         # and also declare under the buffer about the parameters.
 
-
         ob_start();
 
-        foreach ($params as $key => $value) {
-            ${$key} = $value;
+        if ( !empty($params) ) {
+            foreach ($params as $key => $value) {
+                ${$key} = $value;
+            }
         }
 
-        include $c->getCompiledPath($path);
+        include $this->compiler()->getCompiledPath($path);
 
         di()->get('view')->setContent(ob_get_clean());
     }
