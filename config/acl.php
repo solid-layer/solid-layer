d98a35218ee0b0ce90a14388e968fc5d01c04e31
Daison Carino <daison12006013@gmail.com>
2015-10-04 11:00:59 +0000

    removing unused config file; adjusting comments to other files

--- config/acl.php
+++ config/acl.php
@@ -2,9 +2,11 @@
 
 return [
 
+    # ----------------------------------------------------------------
     # Register all the roles you have
-    # ------------------------------------------------------------
+    # ----------------------------------------------------------------
     # - By default we're using 'administrator' and 'user'
+
     'roles'   => [
         'administrator',
         'user',
@@ -12,14 +14,15 @@ return [
 
 
 
+    # ----------------------------------------------------------------
     # Filter Classes
-    # ------------------------------------------------------------
+    # ----------------------------------------------------------------
     # - Register your class to load
+
     'classes' => [
         'default' => Components\Filters\Access::class,
         'csrf'    => Components\Filters\CSRF::class,
         'auth'    => Components\Filters\Auth::class,
     ],
-
 
 ]; # - end of return
