791572672922f003d71591c8e92af00e6373088a
Daison Carino <daison12006013@gmail.com>
2015-10-05 01:27:52 +0000

    changing the responses

--- resources/lang/en/responses/register.php
+++ resources/lang/en/responses/register.php
@@ -5,5 +5,5 @@ return [
         <h4>Great!</h4>
         <p>Now, let\'s try this registration form.</p>
     ',
-    'creation_success'  => 'You have successfully registered, please check your email to activate your account.',
+    'creation_success'  => 'You have successfully registered, please check your mailbox to activate your account.',
 ];
