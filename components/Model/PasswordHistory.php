ac59877dcf767e030dfec0f75a6c18819631b713
Daison Carino <daison12006013@gmail.com>
2015-11-10 12:34:34 +0000

    adding doc block

--- components/Model/PasswordHistory.php
+++ components/Model/PasswordHistory.php
@@ -9,6 +9,12 @@ class PasswordHistory extends Model
     use Timestampable;
     use SoftDeletable;
 
+    /**
+     * By every request, phalcon will always pull this function
+     * as basis to know what is the table's name
+     *
+     * @return string
+     */
     public function getSource()
     {
         return 'password_history';
