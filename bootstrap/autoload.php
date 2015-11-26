04e150561a5bbb82b556c77537424bf30714434c
Daison Carino <daison12006013@gmail.com>
2015-11-26 15:27:33 +0000

    removing the app class and fixing the autoload and compiler class

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,16 +1,33 @@
 <?php
 
+define('SLAYER_START', microtime(true));
+define('BASE_PATH', dirname(__DIR__));
+
 error_reporting(-1);
 
-if ( ! extension_loaded('phalcon')) {
+if ( !extension_loaded('phalcon') ) {
     echo 'Phalcon extension required.'.PHP_EOL;
-
-    exit(1);
+    exit;
 }
 
-define('BASE_PATH', dirname(__DIR__));
-define('SLAYER_START', microtime(true));
 
-require __DIR__ . '/app.php';
+# - call composer autoload to load all the dependencies
 
-$kernel = (new App(BASE_PATH))->bootstrap();
+require BASE_PATH . '/vendor/autoload.php';
+
+
+# - check if there is existing compiled class
+# then require the file.
+
+$compiled = BASE_PATH .'/storage/slayer/compiled.php';
+if ( file_exists($compiled) && php_sapi_name() != 'cli' ) {
+    require $compiled;
+}
+
+
+# - instantiate the kernel class and start loading the configurations
+# and the services.
+
+require BASE_PATH . '/vendor/phalconslayer/framework/src/Kernel.php';
+
+$kernel = (new \Clarity\Kernel(BASE_PATH))->bootstrap();
--- bootstrap/compiler.php
+++ bootstrap/compiler.php
@@ -2,14 +2,39 @@
 
 require dirname(__DIR__) . '/vendor/classpreloader/classpreloader/src/ClassLoader.php';
 
-use ClassPreloader\ClassLoader;
+$config = new \ClassPreloader\Config;
+$loader = new \ClassPreloader\ClassLoader;
 
-$config = ClassLoader::getIncludes(function (ClassLoader $loader) {
-    $loader->register();
 
-    require dirname(__DIR__) . '/bootstrap/autoload.php';
-});
+# - by calling the register(), this acts as a buffer to get all the
+# php files loaded when calling the file autoload.php
 
-$config->addExclusiveFilter('/Swift\/Mime\/SimpleMessage/');
+$loader->register();
+require dirname(__DIR__) . '/bootstrap/autoload.php';
+$loader->unregister();
+
+
+# - the listed php files below are ignored upon running the optimize
+# command.
+#
+# - this refers a namespace closure issues, to make your optimize command
+# work
+
+$ignored = [
+    'symfony/debug/Exception/FlattenException.php',
+    'swiftmailer/swiftmailer/lib/classes/Swift/Mime/SimpleMimeEntity.php',
+];
+
+foreach ( $loader->getFilenames() as $file ) {
+
+    foreach ( $ignored as $ignored_file ) {
+        if ( strpos($file, url_trimmer($ignored_file)) !== false ) {
+            continue 2;
+        }
+    }
+
+    $config->addFile($file);
+}
+
 
 return $config;
