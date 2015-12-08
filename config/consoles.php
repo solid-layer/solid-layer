07b514d48ecf5254afca23aa3551640bb617757b
Daison Carino <daison12006013@gmail.com>
2015-12-08 08:08:01 +0000

    adding consoles and updating phinx directories

--- config/consoles.php
+++ config/consoles.php
@@ -15,7 +15,7 @@ return [
     Clarity\Console\Clear\LogsCommand::class,
     Clarity\Console\Clear\SessionCommand::class,
     Clarity\Console\Clear\ViewsCommand::class,
-    Clarity\Console\DB\SeedCommand::class,
+    Clarity\Console\DB\FactoryCommand::class,
     Clarity\Console\Mail\InlinerCommand::class,
     Clarity\Console\Make\CollectionCommand::class,
     Clarity\Console\Make\ConsoleCommand::class,
@@ -27,6 +27,8 @@ return [
     Clarity\Support\Phinx\Console\Command\Create::class,
     Clarity\Support\Phinx\Console\Command\Migrate::class,
     Clarity\Support\Phinx\Console\Command\Rollback::class,
+    Clarity\Support\Phinx\Console\Command\SeedCreate::class,
+    Clarity\Support\Phinx\Console\Command\SeedRun::class,
     Clarity\Support\Phinx\Console\Command\Status::class,
 
     # - add your console commands below ...
