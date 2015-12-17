533b400167c25f40a367baeada26332b254bf28b
Daison Carino <daison12006013@gmail.com>
2015-12-17 02:13:15 +0000

    removing unnecessary class require; enabling errors

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -3,7 +3,7 @@
 define('SLAYER_START', microtime(true));
 define('BASE_PATH', dirname(__DIR__));
 
-error_reporting(0);
+error_reporting(-1);
 
 if (!extension_loaded('phalcon')) {
     echo 'Phalcon extension required.'.PHP_EOL;
@@ -13,14 +13,16 @@ if (!extension_loaded('phalcon')) {
 
 # - call composer autoload to load all the dependencies
 
-require BASE_PATH . '/vendor/autoload.php';
+require BASE_PATH.'/vendor/autoload.php';
 
 
 # - check if there is existing compiled class
 # then require the file.
 
 $compiled = BASE_PATH .'/storage/slayer/compiled.php';
+
 if (file_exists($compiled) && php_sapi_name() != 'cli') {
+
     require $compiled;
 }
 
@@ -28,6 +30,4 @@ if (file_exists($compiled) && php_sapi_name() != 'cli') {
 # - instantiate the kernel class and start loading the configurations
 # and the services.
 
-require BASE_PATH . '/vendor/phalconslayer/framework/src/Kernel.php';
-
-$kernel = (new \Clarity\Kernel(BASE_PATH))->bootstrap();
+$kernel = new Clarity\Kernel(BASE_PATH);
--- public/index.php
+++ public/index.php
@@ -3,5 +3,6 @@
 require dirname(__DIR__) . '/bootstrap/autoload.php';
 
 $kernel
+    ->modules()
     ->run('main')
     ->render();
