6ca278bec696101f39f43d1dd0b66d60850acb0f
Daison Carino <daison12006013@gmail.com>
2015-12-22 02:58:10 +0000

    transforming mail sender from http load into queuing

--- app/main/controllers/AuthController.php
+++ app/main/controllers/AuthController.php
@@ -8,6 +8,7 @@ use Auth;
 use View;
 use Mail;
 use Lang;
+use Queue;
 use Session;
 use Request;
 use Redirect;
@@ -138,19 +139,15 @@ class AuthController extends Controller
                 'token' => $token,
             ]);
 
-
-            Mail::send('emails.registered-inligned', ['url' => $url],
-                function (\Clarity\Contracts\Mail\MailInterface $mail) use ($inputs) {
-
-                    $mail->to([
-                        $inputs['email'],
-                    ]);
-
-                    $mail->subject(
-                        'You are now registered successfully.'
-                    );
-                }
-            );
+            Queue::put([
+                'class' => 'Components\Queue\Email@registeredSender',
+                'data'  => [
+                    'template' => 'emails.registered-inligned',
+                    'to'       => $inputs['email'],
+                    'url'      => $url,
+                    'subject'  => 'You are now registered, activation is required.'
+                ],
+            ]);
 
             DB::commit();
         } catch (TransactionFailed $e) {
