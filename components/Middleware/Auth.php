b5c48f9f561b5c1302ed9460e99ccca5b5d1e582
Daison Carino <daison12006013@gmail.com>
2016-02-09 01:51:04 +0000

    coding cleanup on middlewares

--- components/Middleware/Auth.php
+++ components/Middleware/Auth.php
@@ -1,25 +1,21 @@
 <?php
 namespace Components\Middleware;
 
-use League\Tactician\Middleware;
-
-class Auth implements Middleware
+class Auth implements \League\Tactician\Middleware
 {
     public function execute($command, callable $next)
     {
-        $di        = $command->getDI();
-        $url       = $di->get('url');
-        $auth      = $di->get('auth');
-        $flash_bag = $di->get('flash_bag');
-        $redirect  = $di->get('redirect');
+        if ( auth()->check() === false ) {
 
-        if ($auth->check() === false) {
-            $flash_bag->error('Please login to access this page.');
+            flashbag()->error('Please login to access this page.');
 
-            $redirect->to(
-                $url->get(
-                    $url->route('showLoginForm'),
-                    ['ref' => $url->current()]
+            redirect(
+
+                url(
+                    route('showLoginForm'),
+                    [
+                        'ref' => url()->current()
+                    ]
                 )
             );
         }
