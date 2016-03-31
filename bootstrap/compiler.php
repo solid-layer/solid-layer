653c9dfa4841970330e802a2e9972677539d88d3
Daison Carino <daison12006013@gmail.com>
2016-03-31 14:00:13 +0000

    enhancing compiler; removing generate-composer

--- bootstrap/compiler.php
+++ bootstrap/compiler.php
@@ -1,21 +1,26 @@
 <?php
 
-# Some classes breaks the compiler, to fix it, you must exclude those files.
-
+# Some classes breaks the compiler, to apply workaround, you must exclude
+# those files.
 $excluded_files = [
     'symfony/debug/Exception/FlattenException.php',
     'swiftmailer/swiftmailer/lib/classes/Swift/Mime/SimpleMimeEntity.php',
 ];
 
 
-
-#-------------------------------------------------------------------------------------------
-# Don't change below, unless you know what you're doing.
-#-------------------------------------------------------------------------------------------
-
-
-
-require dirname(__DIR__).'/vendor/classpreloader/classpreloader/src/ClassLoader.php';
+/*
+ +------------------------------------------------------------------------------
+ | Compiler / Combiner
+ +------------------------------------------------------------------------------
+ |
+ | Below code will list all available classes, the classpreloader package
+ | helps us to do this, it will combine all the classes into one (1)
+ | compiled classes.
+ |
+ | Don't change, unless you know what you're doing.
+ |
+ +------------------------------------------------------------------------------
+ */
 
 $config = new ClassPreloader\Config;
 $loader = new ClassPreloader\ClassLoader;
@@ -23,18 +28,19 @@ $loader = new ClassPreloader\ClassLoader;
 # by calling the register(), this acts as a buffer to get all the
 # php files loaded when calling the file autoload.php
 $loader->register();
-require dirname(__DIR__).'/bootstrap/autoload.php';
+require __DIR__.'/autoload.php';
 $loader->unregister();
 
 foreach ($loader->getFilenames() as $loader_file) {
-    foreach ($excluded_files as $excluded_file) {
-        if (strpos($loader_file, url_trimmer($excluded_file)) !== false) {
+
+    foreach ($excluded_files as $e_file) {
+
+        if (strpos($loader_file, url_trimmer($e_file)) !== false) {
             continue 2;
         }
     }
 
     $config->addFile($loader_file);
 }
-
 
 return $config;
