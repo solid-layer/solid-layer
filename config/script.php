e63f3f0c506fcc088ab6cf9b3654e0b1823e8618
Daison Carino <daison12006013@gmail.com>
2016-04-14 07:51:47 +0000

    removing deploy script, ssh using symfony2 process isn't working

--- config/script.php
+++ config/script.php
@@ -2,15 +2,6 @@
 
 return [
 
-    'deploy' => [
-        'ssh root@website.org',
-        'cd /var/www/phalconslayer/slayer/',
-        'git pull origin master',
-        'php brood db:migrate',
-        'composer update',
-        'composer dumpautoload --optimize',
-    ],
-
     'pull' => [
         'cd ' . BASE_PATH,
         'git pull origin master',
