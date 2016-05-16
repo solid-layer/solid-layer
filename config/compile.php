d134bd771999c1c4df246e73a44b112ff5408d0f
Daison Carino <daison12006013@gmail.com>
2016-05-16 08:05:21 +0000

    making existing lists of compile to be example

--- config/compile.php
+++ config/compile.php
@@ -4,16 +4,16 @@ $base = config('path.root');
 
 return [
     'files' => [
-        $base.'/components/Collection/Collection.php',
-        $base.'/components/Console/Console.php',
-        $base.'/components/Facades/Facade.php',
-        $base.'/components/Model/Model.php',
-        $base.'/components/Providers/ServiceProvider.php',
-        $base.'/components/Validation/Validation.php',
+        // $base.'/components/Collection/Collection.php',
+        // $base.'/components/Console/Console.php',
+        // $base.'/components/Facades/Facade.php',
+        // $base.'/components/Model/Model.php',
+        // $base.'/components/Providers/ServiceProvider.php',
+        // $base.'/components/Validation/Validation.php',
     ],
 
     'folders' => [
-        $base.'/components/',
+        // $base.'/components/',
     ],
 
     'providers' => [
